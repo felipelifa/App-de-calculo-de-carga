efc483dc508f6c0edcc612148b8af5383a5a1090
felipelifa <felipe.delifa@gmail.com>
2026-04-16 21:37:00 -0300

    refactor: sincronizar build web com URL corrigida

--- website/public/treino/flutter_bootstrap.js
+++ website/public/treino/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2724619603" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3705300122" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
