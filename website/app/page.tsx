7bbb22fb45fae6b142d969b0ea9763a1742e36e8
felipelifa <felipe.delifa@gmail.com>
2026-04-22 12:14:26 -0300

    att

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -41,7 +41,7 @@ export default function InitialPage() {
           Sistema de treinamento bio-adaptativo com prescrição científica, periodização DUP e controle real de fadiga.
         </p>
 
-        <Link href="/treino/index.html" style={{
+        <Link href="/treino" style={{
           display: 'inline-flex',
           alignItems: 'center',
           gap: '12px',
