1c0b925e2625db97844f0d62f3c94476287e8e18
felipelifa <felipe.delifa@gmail.com>
2026-08-23 08:50:19 +0100

    feat: add download buttons for Android/iOS + fix GIF display + build APK
    - Updated landing page with Android APK download and iOS placeholder
    - Fixed GIF URL resolution for mobile (use Vercel proxy URL constant)
    - Added gaplessPlayback to all Image.network widgets
    - Removed dead cached_network_image imports
    - Added ExerciseModel.fromMap factory
    - Fixed workouts_api_service.dart import path
    - Added cloud_functions import to pro_service.dart
    - Upgraded Gradle to 9.1.0 for Java 25 compatibility
    - Built optimized APK (65.9MB)

--- app_flutter/lib/core/services/pro_service.dart
+++ app_flutter/lib/core/services/pro_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import '../services/api_service.dart';
 
 class ProService {
